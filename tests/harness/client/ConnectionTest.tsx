a23ae63fdf20c7f53302b7683f7044559bc56741
Tony Hales <email@tonyhales.co.uk>
2026-04-19 23:14:38 +0100

    feat(harness): update ConnectionTest to use connect/disconnect

--- tests/harness/client/ConnectionTest.tsx
+++ tests/harness/client/ConnectionTest.tsx
@@ -16,15 +16,15 @@ const useStyles = createStyles({
 
 export const ConnectionTest = createComponent('ConnectionTest', () => {
   const { css, join } = useStyles();
-  const { onConnectionStateChanged, testDisconnect, testReconnect } = useSocketAPI();
+  const { onConnectionStateChanged, connect, disconnect } = useSocketAPI();
   const [isConnected, setIsConnected] = useState(false);
   onConnectionStateChanged((newIsConnected: boolean) => setIsConnected(newIsConnected));
 
   return (
     <Flex gap={'fields'} disableGrow>
       <Flex className={join(css.connectionStatus, isConnected ? 'socket-connected' : 'socket-disconnected')} />
-      <Button onClick={testDisconnect}>Disconnect</Button>
-      <Button onClick={testReconnect}>Reconnect</Button>
+      <Button onClick={disconnect}>Disconnect</Button>
+      <Button onClick={connect}>Reconnect</Button>
     </Flex>
   );
 });
