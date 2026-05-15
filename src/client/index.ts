43d5ea0200c71dda2f15d21cfbf77d6cf3b32beb
Tony Hales <email@tonyhales.co.uk>
2026-05-16 00:52:50 +0100

    feat(auth): add TokenStorage interface

--- src/client/index.ts
+++ src/client/index.ts
@@ -4,3 +4,4 @@ export { useSocket as useSocketAPI } from './providers';
 export { AuthenticatedOnly, defineAuthentication, useAuthentication, AuthenticationProvider, AuthContext } from './auth';
 export type { ClientUseAuthResult, AuthContextType } from './auth';
 export type { SocketAPIUser } from '../common';
+export type { TokenStorage } from './providers/socket/tokenStorage';
